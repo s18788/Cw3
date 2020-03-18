ecebf247a0d398c792dabdeb359ef09706ef5637
s18788 <s18788@pjwstk.edu.pl>
2020-03-18 01:13:40 +0100

    Add project files.

---
+++ Controllers/StudentsController.cs
@@ -0,0 +1,63 @@
+﻿using Cw3.DAL;
+using Cw3.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace Cw3.Controllers
+{
+    [ApiController]
+    [Route("api/students")]
+    public class StudentsController : ControllerBase
+    {
+        private readonly IDbService _dbService;
+
+
+
+        public StudentsController(IDbService dbService)
+        {
+            _dbService = dbService;
+        }
+
+        [HttpGet]
+        public IActionResult GetStudents(string orderBy)
+        {
+            return Ok(_dbService.GetStudents());
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetStudentId(int id)
+        {
+            if (id == 1)
+            {
+                return Ok("Kowalski");
+            }
+            else if (id == 2)
+            {
+                return Ok("Malewski");
+            }
+            return NotFound("Nie znaleziono studenta");
+        }
+
+        [HttpPost]
+        public IActionResult CreateStudent(Student student)
+        {
+            student.IndexNumber = $"s{new Random().Next(1, 2000)}";
+            return Ok(student);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult PutStudent(int id)
+        {
+            // student.IndexNumber = $"s{new Random().Next(1, 2000)}";
+            return Ok("Aktualizacja ukończona");
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult DeleteStudent(int id)
+        {
+
+            return Ok("Usuwanie dokończone");
+        }
+
+    }
+}
